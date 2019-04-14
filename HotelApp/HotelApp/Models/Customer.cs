c9acdf3746f9435d6c39f41743aa48d0e21d7f0b
Krzysztof Jaworski <krzysiujworski1@gmail.com>
2019-04-14 20:13:39 +0200

    zmieniono plik Customer.cs

--- HotelApp/HotelApp/Models/Customer.cs
+++ HotelApp/HotelApp/Models/Customer.cs
@@ -9,6 +9,8 @@ namespace HotelApp.Models
     {
         public int  Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
